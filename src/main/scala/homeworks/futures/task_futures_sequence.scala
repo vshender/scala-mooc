0b1a0de2664a6e20dc9742d2087281bb1ca7ae5f
Vadim Shender <vadim@shender.org>
2021-01-10 19:08:10 +0300

    Solutions to week 5 homework: Асинхронные операции, обработка исключений, неявные параметры

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -8,18 +8,32 @@ object task_futures_sequence {
 
   /**
    * В данном задании Вам предлагается реализовать функцию fullSequence,
-   * похожую на Future.sequence, но в отличии от нее,
+   * похожую на Future.sequence, но, в отличии от нее,
    * возвращающую все успешные и не успешные результаты.
-   * Возвращаемое тип функции - кортеж из двух списков,
+   * Тип возвращаемого значения функции - кортеж из двух списков,
    * в левом хранятся результаты успешных выполнений,
-   * в правово результаты неуспешных выполнений.
+   * в правом - результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    val resultsF =
+      futures.foldLeft(Future.successful(List[A]())) {
+        case (resultsF, fut) => fut.flatMap(x => resultsF.map(x :: _)).recoverWith(_ => resultsF)
+      }
+    val errorsF =
+      futures.foldLeft(Future.successful(List[Throwable]())) {
+        case (errorsF, fut) => fut.flatMap(_ => errorsF).recoverWith(e => errorsF.map(e :: _))
+      }
+
+    for {
+      results <- resultsF
+      errors <- errorsF
+    } yield (results.reverse, errors.reverse)
+  }
 }

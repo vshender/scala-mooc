c573f94cacd546175ce2ca6bb87dd8cc95e1d3a2
Vadim Shender <vadim@shender.org>
2021-01-10 15:13:51 +0300

    Solutions to week 4 homework: Библиотека коллекций

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -12,20 +12,28 @@ object task_caesar {
    * Сдвиг   - неотрицательное целое число.
    * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
    */
+
   /**
    * @param word   входное слово, которое необходимо зашифровать
    * @param offset сдвиг вперёд по алфавиту
    * @return зашифрованное слово
    */
-  def encrypt(word: String, offset: Int): String =
-    task"Реализуйте метод `encrypt`"()
+  def encrypt(word: String, offset: Int): String = {
+    assert(word.forall((c: Char) => 'A' <= c && c <= 'Z'))
+
+    val normalizedOffset = offset % 26
+    word.map((c: Char) => ((c - 'A' + normalizedOffset) % 26 + 'A').toChar)
+  }
 
   /**
    * @param cipher шифр, который необходимо расшифровать
    * @param offset сдвиг вперёд по алфавиту
    * @return расшифрованное слово
    */
-  def decrypt(cipher: String, offset: Int): String =
-    task"Реализуйте метод `decrypt`"()
+  def decrypt(cipher: String, offset: Int): String = {
+    assert(cipher.forall((c: Char) => 'A' <= c && c <= 'Z'))
 
+    val normalizedOffset = 26 - offset % 26
+    cipher.map((c: Char) => ((c - 'A' + normalizedOffset) % 26 + 'A').toChar)
+  }
 }
